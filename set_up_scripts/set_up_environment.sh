8f64bd162e35060dc8a7501db5809f581e2f6c5b
Tushar Gowda <gowda019@umn.edu>
2022-04-08 22:05:22 -0500

    changing set_up_environment to accomodate for sql script name change

--- set_up_scripts/set_up_environment.sh
+++ set_up_scripts/set_up_environment.sh
@@ -3,7 +3,7 @@
 export SNOWSQL_PRIVATE_KEY_PASSPHRASE='$Snowflake#CSCI_5751_2022!'
 SUPRESS_OUTPUT=false
 
-snowsql -c login -o exit_on_error=true -o quiet=$SUPRESS_OUTPUT -f create_db_scheme_tables.sql
+snowsql -c login -o exit_on_error=true -o quiet=$SUPRESS_OUTPUT -f create_db_schema_tables.sql
 EXIT_CODE=$?
 test $EXIT_CODE -ne 0 && echo "ERROR: create_db_scheme_tables.sql had an error in execution, will not continue. The error code was: $EXIT_CODE" && exit $EXIT_CODE
 
